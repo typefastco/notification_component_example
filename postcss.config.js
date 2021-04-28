13650d2d6ecc4a83b80da9bb8198577c2a31e557
Chris Jeon <chris0374@gmail.com>
2021-04-28 16:44:06 -0500

    Initial commit with tailwindcss and hotwire installed

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+var tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./app/javascript/stylesheets/tailwind.config.js'),
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ]
+}
