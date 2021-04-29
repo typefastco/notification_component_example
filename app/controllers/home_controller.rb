1b299e102e53b50799db5351c4a58631d0073f7c
Chris Jeon <chris0374@gmail.com>
2021-04-28 22:26:53 -0500

    Adds flash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    flash.now[:notice] = {
+      title: "Successfully Sign Up",
+      body: "Welcome to our application"
+    }
   end
 end
